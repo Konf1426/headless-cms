c2a786848ff0af9a15bfd2837b948570f48f09d7
Nahel <nelly.delaruelle@gmail.com>
2024-11-20 10:36:03 +0100

    commands for user managing

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -6,7 +6,6 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Service\Token;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
